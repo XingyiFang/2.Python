012985a932921f990782384069d33d43013eed22
Han Qin <hqin1021@live.com>
2016-06-25 15:02:05 -0400

    Update 2.arcpy-network-analysis.py

--- 2.arcpy-network-analysis.py
+++ 2.arcpy-network-analysis.py
@@ -1,29 +1,31 @@
 __author__ = 'Han'
+#use the code before you change the paths
+#network data is in the data folder
+#purpose: calculate and output shortest paths of each two points.
 import arcpy, datetime, os
 from arcpy import env
 
-mypath = 'C:/Users/hqin/PycharmProjects/include_all_barriers/output/output'+ datetime.datetime.now().strftime("%y-%m-%d_%H_%M")
+mypath = 'C:/Users/.../include_all_barriers/output/output'+ datetime.datetime.now().strftime("%y-%m-%d_%H_%M")
 if not os.path.isdir(mypath):
    os.makedirs(mypath)
-
 try:
     #Check out the Network Analyst extension license
     arcpy.CheckOutExtension("Network")
     #Set environment settings
-    env.workspace = "C:/Users/hqin/PycharmProjects/include_all_barriers/route_exclude_all_428.gdb"
+    env.workspace = "C:/Users/.../include_all_barriers/route_exclude_all_428.gdb"
     env.overwriteOutput = True
     #Set local variables
     inNetworkDataset = "network/network_ND"
     impedanceAttribute = "Length"
     #pairs12102014
-    f = open(r"C:\Users\hqin\PycharmProjects\include_all_barriers\pairs4_0.txt", 'r+')
+    f = open(r"C:\Users\...\include_all_barriers\pairs.txt", 'r+')
     n = 10000
     for line in f:
         n = n - 1
         if n == 0:
             print "10000times"
             n = 10000
-            mypath = 'C:/Users/hqin/PycharmProjects/include_all_barriers/output/output'+ datetime.datetime.now().strftime("%y-%m-%d_%H_%M")
+            mypath = 'C:/Users/.../include_all_barriers/output/output'+ datetime.datetime.now().strftime("%y-%m-%d_%H_%M")
             if not os.path.isdir(mypath):
                os.makedirs(mypath)
             #break
@@ -33,18 +35,15 @@ try:
             print lineArray[1].strip()
             lyr = lineArray[0].strip()
             lyrQuery = lineArray[1].strip()
-            lyrPath = "C:/Users/hqin/PycharmProjects/include_all_barriers/route_exclude_all_428.gdb/Pairs/Pair"
-            arcpy.MakeFeatureLayer_management ("C:/Users/hqin/PycharmProjects/include_all_barriers/route_exclude_all_428.gdb/network/network_ND_Junctions", lyr)
+            lyrPath = "C:/Users/.../include_all_barriers/route_exclude_all_428.gdb/Pairs/Pair"
+            arcpy.MakeFeatureLayer_management ("C:/Users/.../include_all_barriers/route_exclude_all_428.gdb/network/network_ND_Junctions", lyr)
             arcpy.SelectLayerByAttribute_management (lyr, "NEW_SELECTION", lyrQuery)
             arcpy.CopyFeatures_management(lyr, lyrPath)
             
 
             outNALayerName = lyr
             startLocation = "Pairs/Pair"
-            #storeLocations = "Analysis/Stores"
-            #fieldMappings = "Name Name #; Attr_TravelTime ServiceTime #"
             outDirectionsFile = mypath + "/" + outNALayerName + "Directions.txt"
-            #outLayerFile = "C:/data/output" + "/" + outNALayerName + ".lyr"
 
             #Create a new route layer. The route starts at the distribution center and
             #takes the best sequence to visit the store locations.
@@ -64,12 +63,7 @@ try:
             #mappings and search tolerance
             arcpy.na.AddLocations(outNALayer,stopsLayerName,startLocation,"","",
                                   exclude_restricted_elements = "EXCLUDE")
-
-            #Load the store locations as stops. Make sure the store locations are
-            #appended to the Stops sublayer which already contains the distribution
-            #center location. Map the Attr_TravelTime property from the ServiceTime
-            #field so that the total travel time for the route will also contain the
-            #service time using the field mappings
+                                  
             fieldMappings = arcpy.na.NAClassFieldMappings(outNALayer, stopsLayerName)
             fieldMappings["Name"].mappedFieldName = "Name"
             fieldMappings["Attr_" + impedanceAttribute].mappedFieldName = "ServiceTime"
@@ -81,8 +75,6 @@ try:
             arcpy.na.Directions(outNALayer,"TEXT",outDirectionsFile,"Miles",
                                 "REPORT_TIME",impedanceAttribute)
 
-            #Save the solved na layer as a layer file on disk using relative paths
-            #arcpy.SaveToLayerFile_management(outNALayer,outLayerFile,"RELATIVE")
             arcpy.Delete_management(lyrPath)
             print "Script completed successfully"
     f.close()
