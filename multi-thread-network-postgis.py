4ad6f527b4c3d1869a2dc4e3aceafa2f55672247
Han Qin <hqin1021@live.com>
2016-06-25 12:22:24 -0400

    Update and rename network-postgis.py to multi-thread-network-postgis.py

--- multi-thread-network-postgis.py
+++ multi-thread-network-postgis.py
@@ -1,3 +1,6 @@
+#dataset: GMU campus network (over 3700 segements), 2772 junctions. 
+#purpose: every two junctions will create a shortest path, including the passby segements. this script is used to calculate the total count # of each segment of this network. 
+# over 4millions times calculations can be done in 40 minutes.
 __author__ = 'Han'
 import psycopg2,json, sys
 import time
@@ -13,12 +16,11 @@ class ThreadClass(threading.Thread):
     def run(self):
         start_time = time.time()
         try:
-            conn = psycopg2.connect("dbname='network01' user='postgres' port='5432' host='localhost' password='qinhan1021'")
+            conn = psycopg2.connect("dbname='network01' user='postgres' port='5432' host='localhost' password=''")
         except:
             print "I am unable to connect to the database"
         mydict = {}
 
-        #area_model, from 54 - 143
         for i in xrange(self.thread_start, self.thread_end):
             for j in xrange(i, 175):
                 cur = conn.cursor()
@@ -41,7 +43,7 @@ class ThreadClass(threading.Thread):
             conn.commit()
 
         conn.close()
-        json.dump(mydict, open("E:/3. Projects/7. network/python/results.txt", 'a'))
+        json.dump(mydict, open("/results.txt", 'a'))
 
         print("--- %s final seconds ---" % (time.time() - start_time))
 
@@ -52,8 +54,8 @@ class ThreadClass(threading.Thread):
 def test():
     #thread1 = ThreadClass(int(sys.argv[1]), int(sys.argv[2]))
     #thread2 = ThreadClass(int(sys.argv[3]), int(sys.argv[4]))
-	#you can set the variables here, and then run with the commond line as following: the #1,2,3,4 are the input. 
-    #python.exe "e:\3. Projects\7. network\python\network-version2.py" #1 #2 #3 #4
+    #you can set the variables here, and then run with the commond line as following (the #1,2,3,4 are the input) 
+    #python.exe "python\network-version2.py" #1 #2 #3 #4
     thread1 = ThreadClass(1,95)
     thread2 = ThreadClass(95, 175)
     thread1.start()
